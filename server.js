a7a145cbdfbaf065658975e3f2122bd006151a3b
Lupin <mail@lupin.me>
2015-10-29 18:23:55 +0100

    patched server.js. It can now receive raw data

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var net = require('net'),
-    JsonSocket = require('json-socket');
+var net = require('net');
 
+var ip = "0.0.0.0";
 var port = 8000;
+var debug = true
 var server = net.createServer();
-server.listen(port);
-server.on('connection', function(socket) { //This is a standard net.Socket
-    socket = new JsonSocket(socket); //Now we've decorated the net.Socket to be a JsonSocket
 
-    socket._onData = function(data){
-        console.log(data);
-    };
-
-    socket.on('message', function(msg, rinfo) {
-        console.log('Received %d bytes from %s:%d\n',
-            msg.length, rinfo.address, rinfo.port);
+function socket_handler(socket){
+    socket.on('data', function(data){
+	console.log(data.toString());
     });
-});
+}
 
+server.listen(port);
+console.log("Server listening on %s:%d", ip, port);
+server.on('connection', function(socket) {
+    if (debug)
+	console.log("%s connected", socket.remoteAddress);
+    socket_handler(socket);
+});
