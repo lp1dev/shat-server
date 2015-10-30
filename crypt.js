9679b9cade68ac098e1da9e1236aa0b2fea1518d
lp1.eu <mail@lp1.eu>
2015-10-30 16:11:33 +0100

    patched enciphering and deciphering functions

--- crypt.js
+++ crypt.js
@@ -6,9 +6,13 @@ AESCrypt.passphrase = "";
 AESCrypt.iv = "";
 
 AESCrypt.decrypt = function(encryptdata) {
-    var decipher = crypto.createDecipheriv('aes-256-ctr', crypto.createHash('sha256').update(AESCrypt.passphrase).digest(), AESCrypt.iv);
+    encoded = new Buffer(encryptdata, 'base64');
+    console.log(encoded.toString())
+    var cipheredkey = crypto.createHash('sha256').update(AESCrypt.passphrase).digest();
+    console.warn(cipheredkey.toString());
+    var decipher = crypto.createDecipheriv('aes-256-ctr', cipheredkey, AESCrypt.iv);
     return Buffer.concat([
-	decipher.update(encryptdata),
+	decipher.update(encoded),
 	decipher.final()
     ]);
 }
--- server.js
+++ server.js
@@ -3,21 +3,21 @@ var crypt = require("./crypt");
 
 //Config
 var ip = "0.0.0.0";
-var port = 8000;
+var port = 80;
 var debug = true
 var server = net.createServer();
 
-crypt.passphrase = "aaaaaaaaand this is a passphrase";
+crypt.passphrase = "this is a passphrase";
 crypt.iv = "1234567890123456";
 
 var test_message = "Test is OK";
 var encoded_message = crypt.encrypt(test_message);
-console.log(crypt.decrypt(encoded_message));
+console.log(crypt.decrypt(encoded_message).toString('utf-8'));
 
 function socket_handler(socket){
     socket.on('data', function(data){
 	console.log("Raw Data : [%s]", data.toString('utf-8'));
-	console.log("Decyphered Data : [%s]", crypt.decrypt(data.toString('utf-8')));
+	console.log("Decyphered Data : [%s]", crypt.decrypt(data.toString('utf-8')).toString('utf-8'));
     });
 }
 
